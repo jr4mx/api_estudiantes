aa1c8f3784e0b71ca249f250e35b2bfbf3c7e4e0
jr4mx <joserodrigo_ramirez@ucol.mx>
2024-11-08 07:16:42 -0600

    Rutas actualizadas

--- routes/estudiantesRoutes.js
+++ routes/estudiantesRoutes.js
@@ -5,19 +5,18 @@ const estudiantesController = require('../controllers/estudiantesController.');
 
 
 // Ruta para obtener todos los estudiantes
-router.get('/students',estudiantesController.getAllStudents);
+router.get('/estudiantes',estudiantesController.getAllStudents);
 
 // Ruta para obtener estudiantes por estatus
-router.get('/students/status/:estatus', estudiantesController.getStudentsByStatus);
-
+router.get('/estudiantes/status/:estatus', estudiantesController.getStudentsByStatus);
 // Ruta para agregar un nuevo estudiante
-router.post('/students', estudiantesController.addStudent);
+router.post('/estudiante', estudiantesController.addStudent);
 
 // Ruta para modificar un estudiante existente
-router.put('/students/:id', estudiantesController.updateStudent);
+router.put('/actualizar/:id', estudiantesController.updateStudent);
 
 // Ruta para eliminar un estudiante
-router.delete('/students/:id', estudiantesController.deleteStudent);
+router.delete('/borrar/:id', estudiantesController.deleteStudent);
 
 
 module.exports = router;
